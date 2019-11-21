a4852b1cbc3030b770d678c139852ed1e38b8340
ahmed <ahmed)seaf200@yahoo.com>
2019-11-21 17:19:55 +0200

    Add Home Controller Class

---
+++ App/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php 
+
+namespace App\Controllers;
+
+use System\Controller;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+
+       //return $this->view->render('home');
+        //$this->db;
+
+
+
+
+
+       
+    }
+}
